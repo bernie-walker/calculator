c51a524141c0806ccff6de630a87cc7862fe1a44
Bernard Nehal D'Cunha <58025656+bernie-walker@users.noreply.github.com>
2020-02-05 12:01:27 +0530

    added mod

--- calculator.js
+++ calculator.js
@@ -6,13 +6,20 @@ const subtract = function(n1, n2) {
   return n1 - n2;
 };
 
+const mod = function(n1, n2) {
+  if (n1 < n2) return n1;
+
+  return mod(n1 - n2, n2);
+};
+
 const main = function() {
   const [operand1, operand2, sign] = process.argv.slice(2);
   const selectOperation = {
     '+': add,
     '-': subtract,
     '*': multiply,
-    '/': divide
+    '/': divide,
+    '%': mod
   };
   const result = selectOperation[sign](operand1, operand2);
   console.log(result);
